db7bbc1c35eeeaea13cafc4c190c0c4a634849ef
Vaibhav Saxena <vaibhavsaxena2002@gmail.com>
2026-06-27 12:42:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (51.14%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return ans;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+        ans.add(node.val);
+        inorder(node.right);
+    }
+}
