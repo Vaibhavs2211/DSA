06cf177ddcec1389e7a256f1972c84f369cd1174
Vaibhav Saxena <vaibhavsaxena2002@gmail.com>
2026-06-30 16:49:14 +0530

    Time: 3 ms (60.71%) | Memory: 44.2 MB (75.47%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right =  s.length()-1;
+       
+      
+      while(left<right){
+        char leftChar = Character.toLowerCase(s.charAt(left));
+        char rightChar = Character.toLowerCase(s.charAt(right));
+        
+        if(!Character.isLetterOrDigit(leftChar)){
+            left++;
+            continue;
+        }
+        else if(!Character.isLetterOrDigit(rightChar)){
+            right--;
+             continue;
+        }
+        if(leftChar == rightChar){
+           left++;
+           right--;
+       
+        }else{
+            return false;
+           
+        }
+       
+    }
+    return true;
+}
+}
